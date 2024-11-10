9d5aedef6a18853a1140e3924e0b3f927e9f5f7c
Zach <a02104219@usu.edu>
2024-11-10 13:19:16 -0700

    Assignment 9 redo
    Actual Push.  Accidentally committed the wrong files previously.

--- javajam/js/verifysenior.js
+++ javajam/js/verifysenior.js
@@ -3,7 +3,7 @@ var age;
 age = prompt("Enter your age:")
 if (age >= 65) {
     output = "Free Friday Coffee Night for Seniors!" 
-    document.getElementById('verify').style.color = 'maroon';
+    document.getElementById('verify').style.color = 'black';
     document.getElementById('verify').style.fontVariantCaps = 'small-caps';
 } else {
     output = "Enjoy Music and Make Memories!"
